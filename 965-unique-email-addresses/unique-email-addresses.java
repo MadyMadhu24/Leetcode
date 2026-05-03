8839c25b14ea35cffa6172d2c0e65966c914b72f
MadyMadhu24 <madhusharma240605@gmail.com>
2026-05-03 14:40:16 +0530

    Time: 12 ms (72.63%) | Memory: 46.5 MB (63.21%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+        for (String email : emails) {
+            String[] parts = email.split("@");
+            String local = parts[0], domain = parts[1];
+            if (local.contains("+")) local = local.substring(0, local.indexOf('+'));
+            local = local.replace(".", "");
+            set.add(local + "@" + domain);
+        }
+        return set.size();
+    }
+}
