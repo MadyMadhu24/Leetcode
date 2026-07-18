fb32bc15f87c6aa70c5d9f408667ca370f28b8d6
Madhu Sharma <madhusharma240605@gmail.com>
2026-07-19 02:35:22 +0530

    Time: 6 ms (43.19%) | Memory: 44.5 MB (67.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()){
-            return false;
-        }else{
-            int[] count=new int[26];
-            for (int i=0; i<s.length(); i++){
-                count[s.charAt(i)-'a']++;
-            }
-            for (int i=0; i<t.length(); i++){
-                count[t.charAt(i)-'a']--;
-            }
-            for (int i=0; i<count.length; i++){
-                if (count[i]!=0){
-                    return false;
-                }
-            }
-            return true;
 
+        if (s.length() != t.length()) {
+            return false;
         }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int value : count) {
+            if (value != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
